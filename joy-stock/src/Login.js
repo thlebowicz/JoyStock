6ab3fac0ebb52e8345352218c4d3067370b3620e
Tom Hlebowicz <thomas.hlebowicz@gmail.com>
2022-08-06 15:05:22 -0400

    Added MUI and styled login

--- joy-stock/src/Login.js
+++ joy-stock/src/Login.js
@@ -1,7 +1,75 @@
-
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Login() {
-  
+
+  const theme = createTheme();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSignup = () => {};
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth='sm'>
+        <Box sx={{
+          marginTop: '30%',
+          height: 300,
+          border: '1px solid black',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: 3,
+          borderRadius: 2,
+        }}>
+          <Typography variant='h4'>Sign-In</Typography>
+          <Box component="form" onSubmit={handleSubmit}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              label="Password"
+              autoComplete="current-password"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Link 
+              onClick={handleSignup}
+              variant="body2"
+            >
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+
 }
 
 export default Login;
