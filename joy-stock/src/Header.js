8ba2662e971475b590d42e31294c68397930c70e
Tom Hlebowicz <thomas.hlebowicz@gmail.com>
2022-08-06 18:08:27 -0400

    Adding untracked files

---
+++ joy-stock/src/Header.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './Header.css';
+
+
+
+function Header() {
+
+  const navigate = (path) => {};
+
+  return (
+    <AppBar component='nav' sx={{backgroundColor: 'black'}}>
+      <Toolbar>
+        <List sx={{
+          display: 'flex',
+        }}>
+          <ListItem button={true} className='nav-button' onClick={() => navigate('/portfolio')}>
+            Portfolio
+          </ListItem>
+          <ListItem button={true} className='nav-button' onClick={() => navigate('/graph')}>
+            Graph
+          </ListItem>
+          <ListItem button={true} className='nav-button' onClick={() => navigate('/notifications')}>
+            Notifications
+          </ListItem>
+        </List>
+      </Toolbar>
+    </AppBar>
+  );
+
+
+}
+
+export default Header; 
---
+++ joy-stock/src/StockCard.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './StockCard.css';
+import { ExpandMore } from '@mui/icons-material';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CardContent from '@mui/material/CardContent';
+
+function StockCard({stock}) {
+
+  const theme = createTheme();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = () => setExpanded(!expanded);
+
+  return (
+    <div className='main' style={{marginTop: 20}}>
+      <Card 
+        variant='outlined'
+        sx={{
+          backgroundColor: '#1A2027',
+          borderRadius: 5,
+          color: 'white',
+          padding: 2,
+          userSelect: 'none',
+        }}  
+        >
+        <div style={{
+           display: 'flex',
+           alignItems: 'center',
+        }}>
+          <Typography sx={{marginLeft: 5}} variant='h6'>{stock.ticker}</Typography>
+          <Typography sx={{marginLeft: 5}} variant='h7'>${stock.price}</Typography>
+          <Typography sx={{marginLeft: 5}} variant='h7'>{stock.quantity} shares</Typography>
+          <Typography sx={{marginLeft: 5, flexGrow: 1}} variant='h7'>Total holdings: ${stock.quantity * stock.price}</Typography>
+          <IconButton 
+            sx={{
+              backgroundColor: '#969696',
+              '&:hover': {
+                backgroundColor: 'gray',
+              },
+              transform: !expanded ? 'rotate(0deg)' : 'rotate(180deg)',
+              transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+              }),
+            }}
+            onClick={handleExpand}
+          >   
+            <ExpandMore />
+          </IconButton>
+        </div>
+        <Collapse 
+          in={expanded} 
+          orientation="vertical" 
+          timeout="auto" 
+          unmountOnExit
+        >
+        <CardContent>
+          <h1>Bello were gonna make it! </h1>
+        </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  )
+
+}
+
+export default StockCard;
